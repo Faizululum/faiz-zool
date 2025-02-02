9effb53e346774da900398819851fe2f7e2a16f6
Faizululum <faizululum25@gmail.com>
2025-02-02 23:07:42 +0700

    feat: Create Navbar Components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image 
+          src="/icons/logo.svg"
+          alt="logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
+        <p className="text-white font-extrabold max-sm:hidden text-[26px]">Faiz Zool</p>
+      </Link>
+
+      <div className="flex-between gap-5">
+        {/* Clerk - User Management */}
+        <MobileNav />
+      </div>
+    </nav>
   )
 }
 
