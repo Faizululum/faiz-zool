89f65d9248313a812d03c91d7de4ee1d5bf5b84f
Faizululum <faizululum25@gmail.com>
2025-02-14 20:43:58 +0700

    feat: Personal Meeting Room

--- components/MeetingCard.tsx
+++ components/MeetingCard.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import Image from "next/image";
+import { useUser } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
-import { avatarImages } from "@/constants";
 import { useToast } from "@/hooks/use-toast";
 
 interface MeetingCardProps {
@@ -29,6 +29,11 @@ const MeetingCard = ({
   buttonText,
 }: MeetingCardProps) => {
   const { toast } = useToast();
+  const { user } = useUser();
+
+  const userImages = user
+    ? [{ id: user.id, src: user.imageUrl }]
+    : [];
 
   return (
     <section className="flex min-h-[258px] w-full flex-col justify-between rounded-[14px] bg-dark-1 px-5 py-8 xl:max-w-[568px]">
@@ -43,20 +48,22 @@ const MeetingCard = ({
       </article>
       <article className={cn("flex justify-center relative", {})}>
         <div className="relative flex w-full max-sm:hidden">
-          {avatarImages.map((img, index) => (
+          {userImages.map((img, index) => (
             <Image
-              key={index}
-              src={img}
-              alt="attendees"
+              key={img.id}
+              src={img.src}
+              alt="attendee"
               width={40}
               height={40}
               className={cn("rounded-full", { absolute: index > 0 })}
               style={{ top: 0, left: index * 28 }}
             />
           ))}
-          <div className="flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4">
-            +5
-          </div>
+          {userImages.length > 5 && (
+            <div className="flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4">
+              +{userImages.length - 5}
+            </div>
+          )}
         </div>
         {!isPreviousMeeting && (
           <div className="flex gap-2">
